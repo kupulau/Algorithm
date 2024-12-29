4ea24877af37b66de2084dd0cd3f1f62a331d5e1
J. Hwang <89846059+kupulau@users.noreply.github.com>
2024-12-29 14:18:24 +0900

    [Bronze I] Title: 이항 계수 1, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.py
@@ -0,0 +1,13 @@
+n, k = map(int, input().split())
+
+answer = 1
+for i in range(1, n+1):
+    answer *= i
+
+for i in range(1, k+1):
+    answer /= i
+    
+for i in range(1, n-k+1):
+    answer /= i
+
+print(int(answer))
