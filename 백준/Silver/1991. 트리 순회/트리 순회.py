8cc8142cd8586878c253ab7e1df67e42bc7e017d
J. Hwang <89846059+kupulau@users.noreply.github.com>
2025-01-25 14:17:59 +0900

    [Silver I] Title: 트리 순회, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1991. 트리 순회/트리 순회.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+tree = {}
+for _ in range(n):
+    one, two, three = input().split()
+    tree[one] = two, three
+    
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+        
+def inorder(root):
+    if root != ".":
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+        
+def postorder(root):
+    if root != ".":
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+        
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
