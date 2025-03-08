fc607ce7296cc2b0beb44af6f312e405ecda1902
J. Hwang <89846059+kupulau@users.noreply.github.com>
2025-03-08 22:00:37 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 52 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.py
@@ -1,12 +1,16 @@
 import sys
+import heapq
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
 cards = list(map(int, input().split()))
+heapq.heapify(cards)
 
 for _ in range(m):
-    cards.sort()
-    new = cards[0]+cards[1]
-    cards[0], cards[1] = new, new
+    a = heapq.heappop(cards)
+    b = heapq.heappop(cards)
+    new = a + b
+    heapq.heappush(cards, new)
+    heapq.heappush(cards, new)
     
 print(sum(cards))
