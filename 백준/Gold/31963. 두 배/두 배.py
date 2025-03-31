3c396f8bb0e3c225f541943dd11f27d9c8584b02
J. Hwang <89846059+kupulau@users.noreply.github.com>
2025-03-31 11:48:05 +0900

    Revised 두 배.py

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -1,18 +1,16 @@
+import sys
 import math
-import sys; input = sys.stdin.readline
+input = sys.stdin.readline
 
-# 연속된 두 수를 비교하여 조작 횟수를 계산하는 함수
-def sol(arr, n):
-    tmp = 0  # 총 조작 횟수
-    cnt_arr = [0] * n  # 각 인덱스에서의 조작 횟수를 기록하는 배열
-    for i in range(1, n):
-        # 이전 수를 현재 수로 나눈 비율을 기반으로 필요한 2의 제곱 횟수를 계산
-        ratio = math.ceil(math.log2(arr[i - 1] / arr[i])) + cnt_arr[i - 1]
-        if ratio > 0:
-            cnt_arr[i] = max(0, ratio)  # 조작 횟수 계산
-            tmp += cnt_arr[i]  # 총 조작 횟수에 추가
-    return tmp  # 최종 조작 횟수 반환
+n = int(input())
+list1 = list(map(int, input().split()))
 
-n = int(input())  # 수열의 길이 입력
-arr = list(map(int, input().split()))  # 수열의 요소들 입력
-print(sol(arr, n))  # 조작 횟수 계산 결과 출력
+cnt = 0
+cntlist = [0]*n
+for i in range(1, n):
+    ratio = math.ceil(math.log2(list1[i - 1] / list1[i])) + cntlist[i - 1]
+    if ratio > 0:
+        cntlist[i] = max(0, ratio)
+        cnt += cntlist[i]
+    
+print(cnt)
