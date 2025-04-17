6e9aef24e0d5a943e0808fa7d0b116754d9267ee
J. Hwang <89846059+kupulau@users.noreply.github.com>
2025-04-17 22:45:37 +0900

    [Gold III] Title: 세 용액, Time: 2740 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -2,28 +2,27 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
+liquids = sorted(list(map(int, input().split())))
 
-liquids = list(map(int, input().split()))
-liquids.sort()
-
-a, b, c = 0, 0, 0
-minsum = float('inf')
-for i in range(n-2):
-    left = i+1
-    right = n-1
-    while left < right:
-        sum1 = liquids[i]+liquids[left]+liquids[right]
-        
-        if abs(sum1) < abs(minsum):
-            minsum = sum1
-            a, b, c = liquids[i], liquids[left], liquids[right]
-        
-        if sum1 == 0:
-            print(a, b, c)
-            sys.exit(0)
-        elif sum1 > 0:
-            right -= 1
-        else:    # sum1 < 0
-            left += 1
-          
-print(a, b, c)
+def solution():
+    a, b, c = 0, 0, 0
+    minsum = float('inf')
+    for i in range(n-2):
+        left = i+1
+        right = n-1
+        while left < right:
+            sum1 = liquids[i]+liquids[left]+liquids[right]
+            
+            if abs(sum1) < minsum:
+                minsum = abs(sum1)
+                a, b, c = liquids[i], liquids[left], liquids[right]
+            
+            if sum1 == 0:
+                return a, b, c
+            elif sum1 > 0:
+                right -= 1
+            else:    # sum1 < 0
+                left += 1
+    return a, b, c
+    
+print(*solution())
